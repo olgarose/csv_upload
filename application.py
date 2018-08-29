61a80825a4ab56b5cc5f5f9d104d499bcbaed296
olgarose <olgarose@users.noreply.github.com>
2018-08-29 13:21:51 -0700

    Reformat utils usage in app

--- application.py
+++ application.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, g
 from werkzeug.utils import redirect
-from csv_utils import utils
+
+from csv_utils.utils import valid_name, valid_rows, put_csv
 import database as db
 
 application = Flask(__name__)
-
 db.init(application)
 
 
@@ -13,18 +13,13 @@ def index():
     return redirect(url_for('upload'))
 
 
-@application.route('/upload')
-def upload():
-    return render_template('index.html')
-
-
 @application.route('/message', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         uploaded_file = request.files['file']
         lines = uploaded_file.readlines()
-        if utils.valid_name(uploaded_file) and utils.valid_rows(lines):
-            utils.put_csv(lines[1:], db)
+        if valid_name(uploaded_file) and valid_rows(lines):
+            put_csv(lines[1:], db)
             return render_template('thanks.html')
         else:
             return render_template('error.html')
