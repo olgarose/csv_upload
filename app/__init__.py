bc00d957a93dc4f762901f1ca1a91b7a90e5bb43
olgarose <olgarose@users.noreply.github.com>
2018-08-29 21:26:18 -0700

    Change name of config import

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from config import Config
+from app.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
