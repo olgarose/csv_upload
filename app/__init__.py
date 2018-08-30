b48a79784a683f369e54ac56f7d7108df98d2fc2
olgarose <olgarose@users.noreply.github.com>
2018-08-29 21:31:16 -0700

    Put config in app

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from app.config import Config
 
+
+class Config(object):
+    SECRET_KEY = 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = 'mysql://veriadmin:veripassword456@aamcewz99qxfv6.ct2zlshmxr9q.us-east-1.rds.amazonaws.com/ebdb'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
